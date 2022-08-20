9139962f05b9155e60f67cc3249935b60e8b2286
Marilia <marilia.borges01@outlook.com>
2022-08-20 16:40:17 -0300

    Mudando configurações do cypress para o projeto

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        baseUrl: 'https://conexaoqa.herokuapp.com',
+        viewportHeight: 1920,
+        viewportWidth: 1080,
+
+        // eslint-disable-next-line
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
     },
-  },
 });
---
+++ cypress/e2e/api/naoLogado.cy.js
@@ -0,0 +1,7 @@
+describe('API - Profile', () => {
+    context('todos os perfis', () => {
+        it('Valida api de perfis', () => {
+            cy.visit('https://conexaoqa.herokuapp.com/')
+        })
+    })
+})
