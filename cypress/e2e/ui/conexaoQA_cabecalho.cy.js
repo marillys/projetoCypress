28db808fa06a634f53944d0870a7b87ae96f9e3f
Marilia <marilia.borges01@outlook.com>
2022-09-28 08:01:16 -0300

    Validação dos testes de menu usando array

--- cypress/e2e/ui/conexaoQA_cabecalho.cy.js
+++ cypress/e2e/ui/conexaoQA_cabecalho.cy.js
@@ -25,6 +25,37 @@ describe('Cabeçalho home Page', () => {
             cy.getElement('navbar-login')
                 .should('have.attr','href','/login')
                 .and('not.have.attr','target','_blank')
-        })        
+        })   
+        
+        it('Valida o cabeçalho ba area não logada com JSON'), () => {
+            const menus = [
+                { seletor: 'navbar-conexaoQA', link: '/' },
+                { seletor: 'navbar-QAs', link: '/perfis' },
+                { seletor: 'navbar-about', link: '/sobre' },
+                { seletor: 'navbar-register', link: '/cadastrar' },
+                { seletor: 'navbar-login', link: '/login' }
+            ]
+
+            menus.forEach(({ seletor, link }) => {
+                cy.getElement(seletor)
+                    .should('have.attr','href', link)
+                    .and('not.have.attr','target','_blank')
+            })
+        }
+
+        ;[
+            { seletor: 'navbar-conexaoQA', link: '/', nome: ' ConexãoQA' },
+            { seletor: 'navbar-QAs', link: '/perfis',nome: 'QAs' },
+            { seletor: 'navbar-about', link: '/sobre', nome: 'Sobre' },
+            { seletor: 'navbar-register', link: '/cadastrar', nome: 'Cadastrar' },
+            { seletor: 'navbar-login', link: '/login', nome: 'Login' }
+        ].forEach(({seletor, link, nome }) => {
+            it(`Valida o menu ${nome}`, () => {
+                cy.getElement(seletor)
+                    .should('have.attr','href', link)
+                    .and('not.have.attr','target','_blank')
+                    .and('have.text',nome)
+            })
+        })
     })
 })
