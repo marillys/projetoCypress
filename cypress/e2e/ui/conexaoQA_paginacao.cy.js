a79df3229d2fe7edf311e5261b658408fbccb9c4
Marilia <marilia.borges01@outlook.com>
2022-10-13 21:31:46 -0300

    Interceptando e alterando o resultado das API's para validar as paginações da aplicação

--- cypress/e2e/ui/conexaoQA_paginacao.cy.js
+++ cypress/e2e/ui/conexaoQA_paginacao.cy.js
@@ -1,6 +1,63 @@
 describe('Paginação QAs', () => {
     it('Valida 7 perfis', () => {
-        cy.intercept('GET','/api/profile',{ fixture: 'paginacao_7_usuarios.json'})
+        cy.intercept('GET', '/api/profile', { fixture: 'paginacao_7_usuarios.json' })
+            .as('perfis')
+
         cy.visit('/perfis')
+            .wait('@perfis')
+
+        cy.get('.paginationBttns li')
+            .should('not.exist')
+    })
+
+    it('Valida 8 perfis', () => {
+        cy.intercept('GET', '/api/profile', { fixture: 'paginacao_8_usuarios.json' })
+            .as('perfis')
+
+        cy.visit('/perfis')
+            .wait('@perfis')
+
+        const RESULTADO_ESPERADO = ['<', '1', '2', '>']
+
+        cy.get('.paginationBttns li')
+            .should('have.length', RESULTADO_ESPERADO.length)
+            .each((el, index) => {
+                cy.wrap(el)
+                    .should('have.text', RESULTADO_ESPERADO[index])
+            })
+    })
+
+    it('Valida 63 perfis', () => {
+        cy.intercept('GET', '/api/profile', { fixture: 'paginacao_63_usuarios.json' })
+            .as('perfis')
+
+        cy.visit('/perfis')
+            .wait('@perfis')
+
+        const RESULTADO_ESPERADO = ['<', '1', '2', '3', '4', '5', '6', '7', '8', '9', '>']
+
+        cy.get('.paginationBttns li')
+            .should('have.length', RESULTADO_ESPERADO.length)
+            .each((el, index) => {
+                cy.wrap(el)
+                    .should('have.text', RESULTADO_ESPERADO[index])
+            })
+    })
+
+    it('Valida 64 perfis', () => {
+        cy.intercept('GET', '/api/profile', { fixture: 'paginacao_64_usuarios.json' })
+            .as('perfis')
+
+        cy.visit('/perfis')
+            .wait('@perfis')
+
+        const RESULTADO_ESPERADO = ['<', '1', '2', '3', '4', '5', '6', '...', '8', '9', '10', '>']
+
+        cy.get('.paginationBttns li')
+            .should('have.length', RESULTADO_ESPERADO.length)
+            .each((el, index) => {
+                cy.wrap(el)
+                    .should('have.text', RESULTADO_ESPERADO[index])
+            })
     })
 })
