d96b69ceffc369354163e190329b36570b311bc4
Marilia <marilia.borges01@outlook.com>
2022-10-15 20:13:22 -0300

    Ajustes os arquivos da pasta fixture e otimização dos testes que interceptam a API e validam a paginação

--- cypress/e2e/ui/conexaoQA_paginacao.cy.js
+++ cypress/e2e/ui/conexaoQA_paginacao.cy.js
@@ -1,17 +1,30 @@
 describe('Paginação QAs', () => {
+
+    const paginacao = () => cy.get('.paginationBttns li')
+    const URL_API = '/api/profile'
+    const URL_METHOD = 'GET'
+    const validarPaginas = (RESULTADO_ESPERADO) => {
+        paginacao()
+            .should('have.length', RESULTADO_ESPERADO.length)
+            .each((el, index) => {
+                cy.wrap(el)
+                    .should('have.text', RESULTADO_ESPERADO[index])
+            })
+    }
+
     it('Valida 7 perfis', () => {
-        cy.intercept('GET', '/api/profile', { fixture: 'paginacao_7_usuarios.json' })
+        cy.intercept(URL_METHOD, URL_API, { fixture: 'paginacao_7_usuarios.json' })
             .as('perfis')
 
         cy.visit('/perfis')
             .wait('@perfis')
 
-        cy.get('.paginationBttns li')
+        paginacao()
             .should('not.exist')
     })
 
     it('Valida 8 perfis', () => {
-        cy.intercept('GET', '/api/profile', { fixture: 'paginacao_8_usuarios.json' })
+        cy.intercept(URL_METHOD, URL_API, { fixture: 'paginacao_8_usuarios.json' })
             .as('perfis')
 
         cy.visit('/perfis')
@@ -19,16 +32,11 @@ describe('Paginação QAs', () => {
 
         const RESULTADO_ESPERADO = ['<', '1', '2', '>']
 
-        cy.get('.paginationBttns li')
-            .should('have.length', RESULTADO_ESPERADO.length)
-            .each((el, index) => {
-                cy.wrap(el)
-                    .should('have.text', RESULTADO_ESPERADO[index])
-            })
+        validarPaginas(RESULTADO_ESPERADO)
     })
 
     it('Valida 63 perfis', () => {
-        cy.intercept('GET', '/api/profile', { fixture: 'paginacao_63_usuarios.json' })
+        cy.intercept(URL_METHOD, URL_API, { fixture: 'paginacao_63_usuarios.json' })
             .as('perfis')
 
         cy.visit('/perfis')
@@ -36,16 +44,11 @@ describe('Paginação QAs', () => {
 
         const RESULTADO_ESPERADO = ['<', '1', '2', '3', '4', '5', '6', '7', '8', '9', '>']
 
-        cy.get('.paginationBttns li')
-            .should('have.length', RESULTADO_ESPERADO.length)
-            .each((el, index) => {
-                cy.wrap(el)
-                    .should('have.text', RESULTADO_ESPERADO[index])
-            })
+        validarPaginas(RESULTADO_ESPERADO)
     })
 
     it('Valida 64 perfis', () => {
-        cy.intercept('GET', '/api/profile', { fixture: 'paginacao_64_usuarios.json' })
+        cy.intercept(URL_METHOD, URL_API, { fixture: 'paginacao_64_usuarios.json' })
             .as('perfis')
 
         cy.visit('/perfis')
@@ -53,11 +56,22 @@ describe('Paginação QAs', () => {
 
         const RESULTADO_ESPERADO = ['<', '1', '2', '3', '4', '5', '6', '...', '8', '9', '10', '>']
 
-        cy.get('.paginationBttns li')
-            .should('have.length', RESULTADO_ESPERADO.length)
-            .each((el, index) => {
-                cy.wrap(el)
-                    .should('have.text', RESULTADO_ESPERADO[index])
-            })
+        validarPaginas(RESULTADO_ESPERADO)
+    })
+
+    ;[
+        {fixture: 'paginacao_8_usuarios', resultadoEsperado: ['<', '1', '2', '>']},
+        { fixture: 'paginacao_63_usuarios', resultadoEsperado: ['<', '1', '2', '3', '4', '5', '6', '7', '8', '9', '>']},
+        { fixture: 'paginacao_64_usuarios', resultadoEsperado: ['<', '1', '2', '3', '4', '5', '6', '...', '8', '9', '10', '>']}
+    ].forEach(({fixture,resultadoEsperado}) => {
+        it(`Valida a ${fixture}`, () => {
+            cy.intercept(URL_METHOD, URL_API, { fixture })
+                .as('perfis')
+
+            cy.visit('/perfis')
+                .wait('@perfis')
+
+            validarPaginas(resultadoEsperado)
+        })
     })
 })
